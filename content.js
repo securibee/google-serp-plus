672f95e74b6631f36c522af4da95d4b68e69ffc5
securibee <securibee@gmail.com>
2020-03-24 22:51:27 -0400

    chore: use const and let

--- content.js
+++ content.js
@@ -1,18 +1,19 @@
 [...document.querySelectorAll('div.r>a:first-child')].forEach(link => {
-    var url = link.href;
+    const url = link.href;
     console.log({url})
-    var node = document.createElement("p");
-    var textnode = document.createTextNode(url);
+    const node = document.createElement("p");
+    const textnode = document.createTextNode(url);
     node.appendChild(textnode);
+    node.classList.add('full-link')
     link.after(node);
 })
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      doc=document;
-      textarea=doc.createElement("textarea");
-      selection=doc.getSelection();
+      const doc = document;
+      const textarea = doc.createElement("textarea");
+      const selection = doc.getSelection();
       textarea.textContent=[...doc.querySelectorAll("div.r>a:first-child")].map(link=>link.href).join("\n");
       doc.body.appendChild(textarea);
       selection.removeAllRanges();
