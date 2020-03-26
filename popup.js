27ca203c2e4a842ad29894540afc9010150ccf2d
securibee <securibee@gmail.com>
2020-03-25 22:33:08 -0400

    chore: remove console log

--- popup.js
+++ popup.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   }
   document.getElementById('open').onclick = function openUrls() {
-    console.log('open')
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         msg: { action: 'open_urls' }
