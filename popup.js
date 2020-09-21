a6cff3836797ff5d7a49728256b33b20a36d785a
securibee <securibee@gmail.com>
2020-09-21 16:23:04 -0400

    feat: only show buttons when on 'google.com'

--- popup.js
+++ popup.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("copy").onclick = function copyUrls() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        msg: { action: "copy_urls" },
+chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+  if (!tab.url.includes("google.com")) {
+    document.getElementById("none").style.display = "block";
+  } else {
+    document.getElementById("copy").style.display = "block";
+    document.getElementById("open").style.display = "block";
+    document.getElementById("copy").onclick = function copyUrls() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          msg: { action: "copy_urls" },
+        });
       });
-    });
-    window.close();
-  };
-  document.getElementById("open").onclick = function openUrls() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        msg: { action: "open_urls" },
+      window.close();
+    };
+    document.getElementById("open").onclick = function openUrls() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          msg: { action: "open_urls" },
+        });
       });
-    });
-  };
+      window.close();
+    };
+  }
 });
-
